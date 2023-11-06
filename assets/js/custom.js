2e1f2791b66533d052c3a49702e7f09cb98347e6
mediakiller123 <87202119+mediakiller123@users.noreply.github.com>
2023-11-06 10:04:40 +0100

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,188 +1,127 @@
-(function ($) {
-	
+$(document).ready(function(){
 	"use strict";
-	$('.owl-men-item').owlCarousel({
-		items:5,
-		loop:true,
-		dots: true,
-		nav: true,
-		margin:30,
-		  responsive:{
-			  0:{
-				  items:1
-			  },
-			  600:{
-				  items:2
-			  },
-			  1000:{
-				  items:3
-			  }
-		 }
-	})
-
-	$('.owl-women-item').owlCarousel({
-		items:5,
-		loop:true,
-		dots: true,
-		nav: true,
-		margin:30,
-		  responsive:{
-			  0:{
-				  items:1
-			  },
-			  600:{
-				  items:2
-			  },
-			  1000:{
-				  items:3
-			  }
-		 }
-	 })
-
-	$('.owl-kid-item').owlCarousel({
-		items:5,
-		loop:true,
-		dots: true,
-		nav: true,
-		margin:30,
-		  responsive:{
-			  0:{
-				  items:1
-			  },
-			  600:{
-				  items:2
-			  },
-			  1000:{
-				  items:3
-			  }
-		 }
-	 })
-
-	$(window).scroll(function() {
-	  var scroll = $(window).scrollTop();
-	  var box = $('#top').height();
-	  var header = $('header').height();
-
-	  if (scroll >= box - header) {
-	    $("header").addClass("background-header");
-	  } else {
-	    $("header").removeClass("background-header");
-	  }
-	});
-	
-
-	// Window Resize Mobile Menu Fix
-	mobileNav();
+    
+        /*==================================
+* Author        : "ThemeSine"
+* Template Name : Khanas HTML Template
+* Version       : 1.0
+==================================== */
 
 
-	// Scroll animation init
-	window.sr = new scrollReveal();
-	
 
-	// Menu Dropdown Toggle
-	if($('.menu-trigger').length){
-		$(".menu-trigger").on('click', function() {	
-			$(this).toggleClass('active');
-			$('.header-area .nav').slideToggle(200);
-		});
-	}
+/*=========== TABLE OF CONTENTS ===========
+1. Scroll To Top 
+2. Smooth Scroll spy
+3. Progress-bar
+4. owl carousel
+5. welcome animation support
+======================================*/
 
-
-	// Menu elevator animation
-	$('.scroll-to-section a[href*=\\#]:not([href=\\#])').on('click', function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-				var width = $(window).width();
-				if(width < 991) {
-					$('.menu-trigger').removeClass('active');
-					$('.header-area .nav').slideUp(200);	
-				}				
-				$('html,body').animate({
-					scrollTop: (target.offset().top) - 80
-				}, 700);
-				return false;
+    // 1. Scroll To Top 
+		$(window).on('scroll',function () {
+			if ($(this).scrollTop() > 600) {
+				$('.return-to-top').fadeIn();
+			} else {
+				$('.return-to-top').fadeOut();
 			}
-		}
-	});
+		});
+		$('.return-to-top').on('click',function(){
+				$('html, body').animate({
+				scrollTop: 0
+			}, 1500);
+			return false;
+		});
+	
+	
+	
+	// 2. Smooth Scroll spy
+		
+		$('.header-area').sticky({
+           topSpacing:0
+        });
+		
+		//=============
 
-	$(document).ready(function () {
-	    $(document).on("scroll", onScroll);
-	    
-	    //smoothscroll
-	    $('.scroll-to-section a[href^="#"]').on('click', function (e) {
-	        e.preventDefault();
-	        $(document).off("scroll");
-	        
-	        $('.scroll-to-section a').each(function () {
-	            $(this).removeClass('active');
-	        })
-	        $(this).addClass('active');
-	      
-	        var target = this.hash,
-	        menu = target;
-	       	var target = $(this.hash);
-	        $('html, body').stop().animate({
-	            scrollTop: (target.offset().top) - 79
-	        }, 500, 'swing', function () {
-	            window.location.hash = target;
-	            $(document).on("scroll", onScroll);
-	        });
-	    });
-	});
+		$('li.smooth-menu a').bind("click", function(event) {
+			event.preventDefault();
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top - 0
+			}, 1200,'easeInOutExpo');
+		});
+		
+		$('body').scrollspy({
+			target:'.navbar-collapse',
+			offset:0
+		});
 
-	function onScroll(event){
-	    var scrollPos = $(document).scrollTop();
-	    $('.nav a').each(function () {
-	        var currLink = $(this);
-	        var refElement = $(currLink.attr("href"));
-	        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-	            $('.nav ul li a').removeClass("active");
-	            currLink.addClass("active");
-	        }
-	        else{
-	            currLink.removeClass("active");
-	        }
-	    });
-	}
-
-
-	// Page loading animation
-	$(window).on('load', function() {
-		if($('.cover').length){
-			$('.cover').parallax({
-				imageSrc: $('.cover').data('image'),
-				zIndex: '1'
+	// 3. Progress-bar
+	
+		var dataToggleTooTip = $('[data-toggle="tooltip"]');
+		var progressBar = $(".progress-bar");
+		if (progressBar.length) {
+			progressBar.appear(function () {
+				dataToggleTooTip.tooltip({
+					trigger: 'manual'
+				}).tooltip('show');
+				progressBar.each(function () {
+					var each_bar_width = $(this).attr('aria-valuenow');
+					$(this).width(each_bar_width + '%');
+				});
 			});
 		}
+	
+	// 4. owl carousel
+	
+		// i. client (carousel)
+		
+			$('#client').owlCarousel({
+				items:7,
+				loop:true,
+				smartSpeed: 1000,
+				autoplay:true,
+				dots:false,
+				autoplayHoverPause:true,
+				responsive:{
+						0:{
+							items:2
+						},
+						415:{
+							items:2
+						},
+						600:{
+							items:4
 
-		$("#preloader").animate({
-			'opacity': '0'
-		}, 600, function(){
-			setTimeout(function(){
-				$("#preloader").css("visibility", "hidden").fadeOut();
-			}, 300);
-		});
-	});
+						},
+						1199:{
+							items:4
+						},
+						1200:{
+							items:7
+						}
+					}
+				});
+				
+				
+				$('.play').on('click',function(){
+					owl.trigger('play.owl.autoplay',[1000])
+				})
+				$('.stop').on('click',function(){
+					owl.trigger('stop.owl.autoplay')
+				})
 
 
-	// Window Resize Mobile Menu Fix
-	$(window).on('resize', function() {
-		mobileNav();
-	});
+    // 5. welcome animation support
 
+        $(window).load(function(){
+        	$(".header-text h2,.header-text p").removeClass("animated fadeInUp").css({'opacity':'0'});
+            $(".header-text a").removeClass("animated fadeInDown").css({'opacity':'0'});
+        });
 
-	// Window Resize Mobile Menu Fix
-	function mobileNav() {
-		var width = $(window).width();
-		$('.submenu').on('click', function() {
-			if(width < 767) {
-				$('.submenu ul').removeClass('active');
-				$(this).find('ul').toggleClass('active');
-			}
-		});
-	}
+        $(window).load(function(){
+        	$(".header-text h2,.header-text p").addClass("animated fadeInUp").css({'opacity':'0'});
+            $(".header-text a").addClass("animated fadeInDown").css({'opacity':'0'});
+        });
 
-
-})(window.jQuery);
+});	
+	
